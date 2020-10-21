6f7ca75ed097dfbd92ecf57aca4dbb063b648194
Nesaijn <25134837+Nesaijn@users.noreply.github.com>
2020-10-21 10:49:11 +0200

    Update launch.sh
    add support for polybar tray on multi monitor setup

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,9 +6,20 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+# Set some Variables
+OUTPUTS=$(polybar --list-monitors | cut -d":" -f1)
+TRAY_OUTPUT=$(polybar --list-monitors | awk -F: '/primary/{print $1}')
+
 # Launch Polybar, using default config location ~/.config/polybar/config
+
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar --reload main &
+    if [[ $m == $TRAY_OUTPUT ]]
+    then
+      MONITOR=$m TRAY_POSITION=right polybar --reload main &
+    else
+      MONITOR=$m TRAY_POSITION=none polybar --reload main &
+    fi
+
 done
 
 echo "Polybar launched..."
